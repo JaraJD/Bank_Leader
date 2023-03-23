12b19c81e6aa8ad045e787ed4b0ffb1a3cf0eaa3
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-03-23 00:22:36 -0500

    Implementacion API
    Se realiza la implementacion de la capa API creando los controladores para cada entidad

---
+++ Bank.AppService/Controllers/ProductoControlador.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Domain.Entities.Commands;
+using Domain.Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Bank.AppService.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductoControlador
+	{
+		private readonly IProductoCasoDeUso _productoCasoDeUso;
+		private readonly IMapper _mapper;
+
+		public ProductoControlador(IProductoCasoDeUso productoCasoDeUso, IMapper mapper)
+		{
+			_productoCasoDeUso = productoCasoDeUso;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<List<Producto>> Obtener_Listado_Directores()
+		{
+			return await _directorUseCase.ObtenerListaDirectores();
+		}
+
+		[HttpPost]
+		public async Task<Producto> Registrar_Director([FromBody] InsertarNuevoProducto command)
+		{
+			return await _directorUseCase.AgregarDirector(_mapper.Map<Producto>(command));
+		}
+	}
+}
---
+++ Bank.AppService/Middlewares/ErrorHandleMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Bank.AppService.Wrappers;
+
+namespace Bank.AppService.Middlewares
+{
+	public class ErrorHandleMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public ErrorHandleMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (Exception ex)
+			{
+				var response = context.Response;
+				response.ContentType = "application/json";
+
+				var responseModel = new ResponseModel<string>() { Success = false, Message = ex?.Message };
+
+				switch (ex)
+				{
+					
+				}
+}
