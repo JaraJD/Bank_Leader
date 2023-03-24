373ecfaeaae02c00b8d412465aa1633642433178
jmgaviriam <jmgaviriam@gmail.com>
2023-03-24 15:58:39 -0500

    Se implementan los test para TarjetaRepositorio

--- Domain.UseCasesTest/UnitTests/TarjetaRepositorioTest.cs
+++ Domain.UseCasesTest/UnitTests/TarjetaRepositorioTest.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.UseCase.Gateway.Repository;
+using Moq;
 
 namespace Domain.UseCasesTest.UnitTests
 {
-	public class TarjetaRepositorioTest
-	{
-	}
+    public class TarjetaRepositorioTest
+    {
+        private readonly Mock<ITarjetaRepositorio> _tarjetaRepositorio;
+
+        public TarjetaRepositorioTest()
+        {
+
+        }
+    }
 }
